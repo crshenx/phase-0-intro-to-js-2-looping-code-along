a0d2c6a5dffac2bb9c6de66b232db593029d5a18
Chris Hendricks <crshenx@gmail.com>
2022-06-07 19:24:52 -0700

    commit tree 5703934be29bb06dd6aafe1ec46c322ff796a14b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array) {
+  let newArray = [];
+  array.forEach((name) =>
+    newArray.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+  );
+  return newArray;
+}
+
+function countDown(num) {
+  for (let i = num; 0 <= num; num--) {
+    console.log(num);
+  }
+}
